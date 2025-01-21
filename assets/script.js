579aa4917a4ba5ec44c4b495867cdba6a2e874ec
Thiago Sardinha <105871122+thsardinha@users.noreply.github.com>
2025-01-21 20:14:00 -0300

    Site finalizado.

--- assets/script.js
+++ assets/script.js
@@ -13,17 +13,65 @@ window.onscroll = () =>{
 
 /* Slides */
 
-var swiper = new Swiper(".swiper.home-slider", {
+var swiper = new Swiper(".home-slider", {
     spaceBetween: 20,
     effect: "fade",
     grabCursor: true,
-    loop: true,
+    loop:true,
     autoplay: {
-        delay: 2000,
-    },
+        delay: 4000,  
+    }, 
     centeredSlides: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+
+        600: {
+            slidesPerView: 2,
+        },
+    },
+})
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 8000,
+        disableOnInteraction: false,
+    },
+    
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+
+        768: {
+            slidesPerView: 2,
+        },
+
+        991: {
+            slidesPerView: 3,
+        },
     },
 })
