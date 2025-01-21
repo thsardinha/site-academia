abf02be744497cdadfd7a97ab709e65319dac2e8
Thiago Sardinha <105871122+thsardinha@users.noreply.github.com>
2025-01-20 22:48:27 -0300

    CSS finalizado e JS iniciado

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,24 @@ menu.onclick = () =>{
     navbar.classList.toggle('active')
 }
 
+window.onscroll = () =>{
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
+}
+
+/* Slides */
+
+var swiper = new Swiper(".swiper.home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+})
